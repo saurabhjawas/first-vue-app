dabee500331ead33751dce8afaff8de280f3bbbf
Saurabh Sharma <jawas.saurabh@gmail.com>
2020-04-19 01:28:02 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,55 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="6" offset="3">
+          <QuestionBox
+            v-if="questions.length"
+            :currentQuestion="questions[index]"
+            :next="next"
+          />
+        </b-col>
+      </b-row>
+    </b-container>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import QuestionBox from './components/QuestionBox.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    QuestionBox
+  },
+  data() {
+    return {
+      questions: [],
+      index: 0
+    }
+  },
+  methods: {
+    next: function() {
+      if (this.index < 9) {
+        this.index = this.index + 1
+      }
+    }
+  },
+  mounted: function() {
+    fetch('https://opentdb.com/api.php?amount=10&category=27&type=multiple', {
+      method: 'get'
+    })
+    .then(response => response.json())
+    .then(data => {
+      // console.log('here is data: ', data)
+      this.questions = data.results
+      // console.dir(this);
+    })
   }
 }
 </script>
---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="question-box-container">
+    <b-jumbotron>
+    <!-- <template v-slot:header>BootstrapVue</template> -->
+
+    <template v-slot:lead>
+      {{ currentQuestion.question }}
+    </template>
+
+    <hr class="my-4">
+
+
+    <p v-for="(answer, index) in answers" :key="index">
+      {{ answer }}
+    </p>
+
+    <b-button variant="primary" href="#">Submit</b-button>
+    <b-button variant="success" href="#"
+      @click="next"
+    >
+      Next Question
+    </b-button>
+  </b-jumbotron>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    currentQuestion: Object,
+    next: Function
+  },
+  computed: {
+    answers() {
+      return [ ...this.currentQuestion.incorrect_answers , this.currentQuestion.correct_answer]
+    }
+  }
+}
+</script>
